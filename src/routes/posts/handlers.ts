d4c8023eb05139d456603f3ffb933a6cf49fe2c5
Fauzan FL <fauzanarth@gmail.com>
2024-08-11 22:20:43 +0700

    chore: complete functionallity

--- src/routes/posts/handlers.ts
+++ src/routes/posts/handlers.ts
@@ -1,3 +1,4 @@
+import { NotFoundError } from 'elysia';
 import db from '../../db'
 
 export async function getPosts() {
@@ -5,5 +6,41 @@ export async function getPosts() {
         return await db.post.findMany({orderBy: {createdAt: 'asc'}});
     } catch (error) {
         console.error(error);
+    }
+}
+
+export async function getPostById(id: number) {
+    try {
+        const post = db.post.findUnique({where: {id}});
+        if (!post) {
+            throw new NotFoundError('Post not found');
+        }
+        return post;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function createPost(data: {title: string, content: string}) {
+    try {
+        return await db.post.create({data});
+    } catch (error) {
+        console.error(`Error creating post: ${error}`);
+    }
+}
+
+export async function updatePost(id: number, data: {title?: string, content?: string}) {
+    try {
+        return await db.post.update({where: {id}, data});
+    } catch (error) {
+        console.error(`Error updating post: ${error}`);
+    }
+}
+
+export async function deletePost(id: number) {
+    try {
+        return await db.post.delete({where: {id}});
+    } catch (error) {
+        console.error(`Error deleting post: ${error}`);
     }
 }
--- src/routes/posts/index.ts
+++ src/routes/posts/index.ts
@@ -1,11 +1,38 @@
-import {Elysia} from "elysia";
-import { getPosts } from "./handlers";
+import {Elysia, t} from "elysia";
+import { createPost, deletePost, getPostById, getPosts, updatePost } from "./handlers";
 
 const postRoutes = new Elysia({prefix: "/posts"})
 .get("/", () => getPosts())
-.get("/:id", ({params}) => `Get post with id ${params.id}`)
-.post("/", () => "Create a new post")
-.put("/:id", ({params}) => `Update post with id ${params.id}`)
-.delete("/:id", ({params}) => `Delete post with id ${params.id}`);
+.get("/:id", ({params: {id}}) => getPostById(id), {
+    params: t.Object({id: t.Number()})
+})
+.post("/", ({body}) => createPost(body), {
+    body: t.Object({
+        title: t.String({
+            minLength: 5,
+            maxLength: 100
+        }),
+        content: t.String({
+            minLength: 10
+        })
+    })
+})
+.put("/:id", ({params: {id}, body}) => updatePost(id, body), {
+    params: t.Object({id: t.Number()}),
+    body: t.Object({
+        title: t.Optional(t.String({
+            minLength: 5,
+            maxLength: 100
+        })),
+        content: t.Optional(t.String({
+            minLength: 10
+        }))
+    }, {
+        minProperties: 1
+    })
+})    
+.delete("/:id", ({params: {id}}) => deletePost(id), {
+    params: t.Object({id: t.Number()})
+});
 
 export default postRoutes;
